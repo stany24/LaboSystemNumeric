e50b986f8b296430e505076cccd2b2feb5c50117
Gouvernon <stan@gouvernon.org>
2024-04-09 09:01:18 +0200

    created testbench

--- Rendu_2/Vivado/Vivado.srcs/sources_1/imports/design.hdl/ROM.vhd
+++ Rendu_2/Vivado/Vivado.srcs/sources_1/imports/design.hdl/ROM.vhd
@@ -42,7 +42,7 @@ begin
 
 
 with pc_i select
-    ir_o <= -- début du programme en adresse 0
+    ir_o <=
          -- X"10" load dilswitch 1
          -- X"10" store bargraph 1
          -- X"11" load dilswitch 2
@@ -55,11 +55,14 @@ with pc_i select
          BZ0 & x"06" when X"03", -- saut si non égal à 0
          -- if
          LOADaddr & X"E0" when X"04", -- charge la valeur depuis la ram
-         STOREaddr & X"10" when x"05", -- affiche la valeur sur le bargraph
+         STOREaddr & X"10" when x"05", -- affiche la valeur sur le bargraph 1
+         
          -- else
          XORaddr & X"E0" when X"06", -- Xor des deux valeurs
-         STOREaddr & X"10" when x"07", -- affiche la valeur sur le bargraph
+         STOREaddr & X"10" when x"07", -- affiche la valeur sur le bargraph 1
+         LOADconst & "0011110" when x"08", -- charge la valeur de c
+         STOREaddr & X"11" when x"09", -- affiche c sur l'affichage 7seg
          -- end if
-         BRA & X"00" when others; -- Boucle à l'adresse X"0C" 
+         BRA & X"00" when others; -- Boucle à l'adresse X"00" 
           
 end architecture Behavioral ; -- of ROM
